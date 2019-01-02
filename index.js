6f547874b084410de2f172d38b5c880ad880fb0c
tiot07 <tiot7155@gmail.com>
2019-01-03 02:23:01 +0900

    投稿結果表示

--- index.js
+++ index.js
@@ -14,13 +14,14 @@ const server = http.createServer((req, res) => {
 			rs.pipe(res);
 			break;
 		case 'POST':
-			res.write('POST ' + req.url);
 			let body = [];
 			req.on('data', (chunk) => {
 				body.push(chunk);
 			}).on('end', () => {
 				body = Buffer.concat(body).toString();
-				console.info('[' + now + '] Data posted: ' + body);
+				const decord = decodeURIComponent(body);
+				console.info('['+now+'] 投稿: '+decoded);
+				res.write('<!DOCTYPE html><html lang="ja"><body><h1>'+decoded+'が投稿されました</h1></body></html>');
 				res.end();
 			});
 			break;
