0daf5e90138c28110faf315d7a776383ccff16f0
tiot07 <tiot7155@gmail.com>
2019-01-03 02:27:11 +0900

    コピペ

--- index.js
+++ index.js
@@ -19,10 +19,11 @@ const server = http.createServer((req, res) => {
 				body.push(chunk);
 			}).on('end', () => {
 				body = Buffer.concat(body).toString();
-				const decord = decodeURIComponent(body);
-				console.info('[' + now + '] 投稿: ' + decoded);
-				res.write('<!DOCTYPE html><html lang="ja"><body><h1>'+decoded+'が投稿されました</h1></body></html>');
-				res.end();
+            const decoded = decodeURIComponent(body);
+            console.info('[' + now + '] 投稿: ' + decoded);
+            res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
+                decoded + 'が投稿されました</h1></body></html>');
+            res.end();
 			});
 			break;
 		default:
